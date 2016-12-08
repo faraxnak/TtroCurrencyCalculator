f3fa1b2510dce6a251350dbada92e32dc22c82e1
faraxnak <farid@farids-macbook-pro.local>
2016-12-08 11:31:46 +0330

    Initial Commit

---
+++ TtroCurrencyCalculator/TtroCurrencyCalculator.h
@@ -0,0 +1,19 @@
+//
+//  TtroCurrencyCalculator.h
+//  TtroCurrencyCalculator
+//
+//  Created by Farid on 12/8/16.
+//  Copyright © 2016 ParsPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TtroCurrencyCalculator.
+FOUNDATION_EXPORT double TtroCurrencyCalculatorVersionNumber;
+
+//! Project version string for TtroCurrencyCalculator.
+FOUNDATION_EXPORT const unsigned char TtroCurrencyCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TtroCurrencyCalculator/PublicHeader.h>
+
+
