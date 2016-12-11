946c7eff073520f1490810477aee1a6cf5df4e29
faraxnak <farid@Farids-MacBook-Pro.local>
2016-12-11 14:45:56 +0330

    second commit

--- Pods/Target Support Files/Pods-TtroCurrencyCalculatorTests/Pods-TtroCurrencyCalculatorTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_TtroCurrencyCalculatorTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TtroCurrencyCalculatorTestsVersionString[];
-
---
+++ build/TtroCurrencyCalculator.build/Release-iphoneos/TtroCurrencyCalculator.build/Script-7B351A85195CC4B65A477A39.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-TtroCurrencyCalculator/Pods-TtroCurrencyCalculator-resources.sh"
+
